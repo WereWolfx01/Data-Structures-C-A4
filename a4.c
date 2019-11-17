c4a2f70a0b6cb538b87e929285c332d7aa8e9246
Aeman Abdulmuhssen <aabdulmu@fred.socs.uoguelph.ca>
2019-11-17 15:06:47 -0500

    backup push, all three hash function + 1 helper function

---
+++ a4.c
@@ -0,0 +1,85 @@
+/*
+Aeman Abdulmuhssen
+Student ID: 1003984
+E-mail: aabdulmu@uoguelph.ca
+*/
+
+
+#include "a4.h"
+
+int pow1( int base, int exp ){
+  int result = base;
+
+  if(exp == 1){
+    return result;
+  }
+
+  if(exp == 0){
+    return 1;
+  }
+
+  while( exp > 1 ){
+  result = result * result;
+  exp--;
+  }
+  return result;
+}
+
+
+int hash1( char *s, int max ){
+  char *c;
+  unsigned long result;
+  int i;
+
+  result = 0;
+  i = 1;
+
+  for(c=s; (*c); c++ ){
+    result += (( *c ) * pow1(19,i)); /*each character is multiplied by a constant to the power of character's position*/
+    i++;
+  }
+
+  return result % max;
+}
+
+int hash2( char *s, int max ){
+  char *c;
+  unsigned long result;
+  int i;
+  int j;
+
+  result = 0;
+  i = 1;
+  j = 1;
+
+  for(c=s; (*c); c++ ){
+    result += (( *c ) * pow1(19,i)) * 7 ;
+    result += atoi(c) * j;
+    j = j * 7;
+    i++;
+  }
+
+  return result % max;
+}
+
+
+int hash3( char *s, int max ){
+  int dayspermonth[] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+  int numMonths = atoi(s);
+  int numDays = atoi(s+3);
+  int numYear = atoi(s+6);
+  int result = 0;
+  if(numMonths){
+  numMonths--;
+  while( numMonths != 0 ){
+    result += dayspermonth[numMonths];
+    numMonths--;
+  }
+  result += numDays;
+  result += ((numYear - 1900) * 366); /*1900 was the lowest year in the file, each year has its own 366 indexes in hash map*/
+  }
+
+  /*some years have februray with 29 days so 366 days a year*/
+
+  return result % max;
+}
---
+++ a4.h
@@ -0,0 +1,13 @@
+/*
+Aeman Abdulmuhssen
+Student ID: 1003984
+E-mail: aabdulmu@uoguelph.ca
+*/
+
+#include "stdlib.h"
+
+
+int pow1( int base, int exp ); /*alternate version of pow(int base, int exp)*/
+int hash1( char *s, int max );
+int hash2( char *s, int max );
+int hash3( char *s, int max );
